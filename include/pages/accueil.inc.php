b4a7ce5a7b234494f893c23616fbf838ccadff90
Guillaume Rousselet <guillaume.rousselet11@gmail.com>
2019-12-21 18:54:29 +0100

    ajout gif

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -1,4 +1,4 @@
-<a href="https://i.pinimg.com/originals/f8/09/35/f809352a639eba66e1499c8c97f6e582.gif"><img class="centreImage" src="image/logo.gif" alt="Bétisier IUT" title="On se fend la poire ici !!"/></a>
+<a href="image/mdr.gif"><img class="centreImage" src="image/logo.gif" alt="Bétisier IUT" title="On se fend la poire ici !!"/></a>
 
-<!--Vrai ligne de code ci-dessous https://media3.giphy.com/media/ZqlvCTNHpqrio/giphy.gif-->
+<!--Vrai ligne de code ci-dessous-->
 <!--<img class="centreImage" src="image/logo.gif" alt="Bétisier IUT" title="Bétisier IUT Limousin"/>-->
